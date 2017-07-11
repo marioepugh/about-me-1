89b76fa6a755d1ac49909b9d1c7c011067b75fcc
daniel <dpillay03@gmail.com>
2017-07-11 16:31:06 -0700

    i have updated my app.js index.html. style.css

---
+++ app.js
@@ -0,0 +1,54 @@
+var question1= prompt('As a student, I want to learn to code and pass code201 so I can move on to code 301');
+
+if(question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y'){
+  alert('Congrats');
+  console.log('User answered correctly');
+}
+else{
+  alert('Wrong');
+  console.log('User answered wrong');
+}
+
+var question1= prompt('As a student, I want to add on to continue to add on to my portfolio to show steady growith');
+
+if(question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y'){
+  alert('Congrats');
+  console.log('User answered correctly');
+}
+else{
+  alert('Wrong');
+  console.log('User answered wrong');
+}
+
+var question1= prompt('As a human, I strive to be in good physical conditioning because it helps provides me with mental clarity');
+
+if(question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y'){
+  alert('Congrats');
+  console.log('User answered correctly');
+}
+else{
+  alert('Wrong');
+  console.log('User answered wrong');
+}
+
+var question1= prompt('As a student, I will always copy and paste code from online because its easy and I can get things done faster');
+
+if(question1.toUpperCase() === 'NO' || question1.toUpperCase() === 'N'){
+  alert('Congrats');
+  console.log('User answered correctly');
+}
+else{
+  alert('Wrong');
+  console.log('User answered wrong');
+}
+
+var question1= prompt('As a student, I strive to graduate codefellows and become a full stack javascript developer so I can begin my career');
+
+if(question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'YES'){
+  alert('Congrats');
+  console.log('User answered correctly');
+}
+else{
+  alert('Wrong');
+  console.log('User answered wrong');
+}
